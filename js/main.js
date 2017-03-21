32b8279f44a3b6077092b4f8ce178e6a2817f957
Michael Stashevsky <mstashev@gmail.com>
2017-03-21 15:23:54 -0400

    Explorer mode finished

--- js/main.js
+++ js/main.js
@@ -1,64 +1,432 @@
-const imdb = require('imdb-api');
-
-
-
-
-
-
-
 var favMovies = []
 
-var movie1 = [
-  title,
-  director_list,
-  genre,
-  year,
-  actor_list
-]
+favMovies.push({
+  title: "Deadpool",
+  director_list: [
+    "Tim Miller"
+  ],
+  genre: [
+    "Action",
+    "Adventure",
+    "Comedy"],
+  year: "2016",
+  actor_list: [
+    "Ryan Reynolds",
+    "Karan Soni",
+    "Ed Skrein",
+    "Michael Benyaer",
+    "Stefan Kapicic",
+    "Brianna Hildebrand",
+    "Style Dayne",
+    "Kyle Cassie",
+    "Taylor Hickson",
+    "Naika Toussaint",
+    "Randal Reeder",
+    "T.J. Miller",
+    "Isaac C. Singleton Jr.",
+    "Morena Baccarin",
+    "Justyn Shippelt",
+    "Donna Yamamoto",
+    "Jed Rees",
+    "Hugh Scott",
+    "Gina Carano",
+    "Cindy Piper",
+    "Emily Haine",
+    "Aatash Amir",
+    "Chad Riley",
+    "Paul Belsito",
+    "Darcey Johnson",
+    "Leslie Uggams",
+    "Kyle Rideout",
+    "Jason William Day",
+    "Stan Lee",
+    "Ben Wilkinson",
+    "Rachel Sheen",
+    "Paul Lazenby",
+    "Rob Hayter"]
+})
 
-var movie2 = [
-  title,
-  director_list,
-  genre,
-  year,
-  actor_list
-]
+favMovies.push({
+  title: "Logan",
+  director_list: [
+    "James Mangold"
+  ],
+  genre: [
+    "Action",
+    "Drama",
+    "Sci-Fi"
+  ],
+  year: "2017",
+  actor_list: [
+    "Hugh Jackman",
+    "Patrick Stewart",
+    "Dafne Keen",
+    "Boyd Holbrook",
+    "Stephen Merchant",
+    "Elizabeth Rodriguez",
+    "Richard E. Grant",
+    "Eriq La Salle",
+    "Elise Neal",
+    "Quincy Fouse",
+    "Nate Munson",
+    "Al Coronel",
+    "Frank Gallegos",
+    "Anthony Escobar",
+    "Reynaldo Gallegos",
+    "Krzysztof Soszynski",
+    "Stephen Dunlevy",
+    "Daniel Bernhardt",
+    "Ryan Sturz",
+    "Brandon Melendy",
+    "Jeremy Fitzgerald",
+    "Luke Hawx",
+    "Chris Palermo",
+    "Paul Andrew O'Connor",
+    "Clinton Roberts",
+    "Rocky Abou-Sakher",
+    "Keith Jardine",
+    "Jean Claude Leuyer",
+    "Andrew Arrabito",
+    "Jef Groff",
+    "Sebastian James",
+    "Aaron Matthews",
+    "Garrett Hammond",
+    "Matthew McClain",
+    "Maureen Brennan",
+    "Jason Genao",
+    "Hannah Westerfield",
+    "Bryant Tardy",
+    "Ashlyn Casalegno",
+    "Alison Fernandez",
+    "Parker Lovein",
+    "Jimmy Gonzales",
+    "Dave Davis",
+    "Lennie Loftin",
+    "Mark Ashworth",
+    "James Handy",
+    "Bryce Romero",
+    "Phi Vu",
+    "Chester Rushing",
+    "David Simpson",
+    "Lauren Gros",
+    "John Raymond",
+    "Vanessa Cloke",
+    "Doris Morgado",
+    "Katie Anne Mitchell",
+    "Lara Grice",
+    "James Moses Black",
+    "Ned Yousef",
+    "Michael Lehr",
+    "Baxter Humby",
+    "Bryan Sloyer",
+    "Daniel Hernandez",
+    "John Bernecker",
+    "Evan D. Taylor",
+    "Joe Nin Williams",
+    "Toby Holguin",
+    "Robert Wu",
+    "Panuvat Anthony Nanakornpanom",
+    "Victor Winters-Junco",
+    "Eyad Elbitar",
+    "Craig Henningsen",
+    "Han Soto",
+    "Rissa Kilar",
+    "Salef Celiz",
+    "Aidan Kennedy",
+    "Nayah Murphy",
+    "Chase Cubia",
+    "Emma Teo",
+    "Vincenzo Lucatorto",
+    "Noell Jellison",
+    "Haley Glass",
+    "Ella Rowbotham",
+    "Hudson Wright",
+    "Sebeon Jackson",
+    "Kelton DuMont",
+    "Damon Carney",
+    "Cynthia Woods",
+    "Mali O'Connor",
+    "David Kallaway",
+    "Robert Vargas",
+    "David Paris"
+  ]
+})
 
-var movie3 = [
-  title,
-  director_list,
-  genre,
-  year,
-  actor_list
-]
+favMovies.push({
+  title: "Guardians of the Galaxy, Vol. 1",
+  director_list: [
+    "James Gunn"
+  ],
+  genre: [
+    "Action",
+    "Adventure",
+    "Sci-Fi"
+  ],
+  year: "2014",
+  actor_list: [
+    "Chris Pratt",
+    "Zoe Saldana",
+    "Dave Bautista",
+    "Vin Diesel",
+    "Bradley Cooper",
+    "Lee Pace",
+    "Michael Rooker",
+    "Karen Gillan",
+    "Djimon Hounsou",
+    "John C. Reill",
+    "Glenn Close",
+    "Benicio Del Toro",
+    "Laura Haddock",
+    "Sean Gunn",
+    "Peter Serafinowicz",
+    "Christopher Fairbank",
+    "Krystian Godlewski",
+    "Wyatt Oleff",
+    "Gregg Henry",
+    "Janis Ahern",
+    "Solomon Mousley",
+    "Lindsay Morton",
+    "Robert Firth",
+    "Melia Kreiling",
+    "Tom Proctor",
+    "Nick Holmes",
+    "Max Wrottesley",
+    "Stan Lee",
+    "Nicole Alexandra Shipley",
+    "Sharif Atkins",
+    "Brendan Fehr",
+    "Tomas Arana",
+    "Mikaela Hoover",
+    "Emmett J Scanlan",
+    "Dominic Grant",
+    "Spencer Wilding",
+    "Alison Lintott",
+    "Alexis Rodney",
+    "Nathan Fillion",
+    "Keeley Forsyth",
+    "Frank Gilhooley",
+    "Alexis Denisof",
+    "Enzo Cilenti",
+    "Richard Katz",
+    "Enoch Frost",
+    "Ronan Summers",
+    "Ophelia Lovibond",
+    "Laura Ortiz",
+    "Marama Corlett",
+    "Rosie Jones",
+    "Abidemi Sobande",
+    "Alex Rose",
+    "Ekaterina Zalitko",
+    "Emily Redding",
+    "Stephen Blackehart",
+    "Jennifer Moylan-Taylor",
+    "Bruce Mackinnon",
+    "Ralph Ineson",
+    "Rob Zombie",
+    "Naomi Ryan",
+    "John Brotherton",
+    "Graham Shiels",
+    "James Gunn",
+    "Douglas Robson",
+    "Rachel Cullen",
+    "Isabella Poynton",
+    "Imogen Poynton",
+    "David Yarovesky",
+    "Miriam Lucia"
+  ]
+})
 
-var movie4 = [
-  title,
-  director_list,
-  genre,
-  year,
-  actor_list
-]
+favMovies.push({
+  title: "X-Men: Apocalypse",
+  director_list: [
+    "Bryan Singer"
+  ],
+  genre: [
+    "Action",
+    "Adventure",
+    "Sci-Fi"
+  ],
+  year: "2016",
+  actor_list: [
+    "James McAvoy",
+    "Michael Fassbender",
+    "Jennifer Lawrence",
+    "Nicholas Hoult",
+    "Oscar Isaac",
+    "Rose Byrne",
+    "Evan Peters",
+    "Josh Helman",
+    "Sophie Turner",
+    "Tye Sheridan",
+    "Lucas Till",
+    "Kodi Smit-McPhee",
+    "Ben Hardy",
+    "Alexandra Shipp",
+    "Lana Condor",
+    "Olivia Munn",
+    "Warren Scherer",
+    "Rochelle Okoye	",
+    "Monique Ganderton",
+    "Fraser Aitcheson",
+    "Abdulla Hamam",
+    "Hesham Hammoud",
+    "Antonio Daniel Hidalgo",
+    "Al Maini",
+    "Berdj Garabedian",
+    "Ally Sheedy",
+    "Anthony Konechny",
+    "Emma Elle Paterson",
+    "Manuel Sinor",
+    "Giant Gustav Claude Ouimet",
+    "Lukas Penar",
+    "Ryan Hollyman",
+    "Joanne Boland",
+    "Nabeel El Khafif",
+    "Manuel Tadros",
+    "Abanoub Andraous",
+    "Aladeen Tawfeek",
+    "Carolina Bartczak",
+    "T.J. McGibbon",
+    "Davide Chiazzese",
+    "Sebastian Naskrent",
+    "Boris Sichon",
+    "Martin Skorek",
+    "Kamil Orzechowski",
+    "Michael Terlecki",
+    "Ahmed Osman",
+    "Ziad Ghanem",
+    "Moataz Fathi",
+    "Tómas Lemarquis",
+    "James Loye",
+    "Zehra Leverman",
+    "Herb Luft",
+    "Stan Lee",
+    "Joan Lee",
+    "Stephen Bogaert",
+    "John Bourgeois",
+    "Conrad Coates",
+    "Dan Lett",
+    "Adrian G. Griffiths",
+    "Shawn Campbell",
+    "Joe Cobden",
+    "Henry Hallowell",
+    "Danièle Dury",
+    "Naomi Frenette",
+    "Aj Risi",
+    "Raphaël Dury",
+    "Ian Rosenberg",
+    "Erika Heather Mergl",
+    "Mary-Pier Gaudet",
+    "Josh Madryga",
+    "Scott Cook",
+    "Allen Keng",
+    "Tally Rodin",
+    "Francis Limoges",
+    "Tsu-Ching Yu",
+    "Karl Walcott",
+    "Desmond Campbell",
+    "Ian Geldart",
+    "John Ottman",
+    "Linda Joyce Nourse",
+    "Zeljko Ivanek",
+    "Christopher B. MacCabe",
+    "Chris Cavener",
+    "Ronald Tremblay",
+    "Joseph Bellerose",
+    "Philippe Hartmann",
+    "Sebastien R. Teller",
+    "Alexander Peganov",
+    "Simon Therrien",
+    "Patrice Martre",
+    "James Malloch"
+  ]
+})
 
-var movie5 = [
-  title,
-  director_list,
-  genre,
-  year,
-  actor_list
-]
+favMovies.push({
+  title: "Suicide Squad",
+  director_list: [
+    "David Ayer"
+  ],
+  genre: [
+    "Action",
+    "Adventure",
+    "Fantasy"
+  ],
+  year: "2016",
+  actor_list: [
+    "Will Smith",
+    "Jaime FitzSimons",
+    "Ike Barinholtz",
+    "Margot Robbie",
+    "Christopher Dyson",
+    "Bambadjan Bamba",
+    "Viola Davis",
+    "Ted Whittall",
+    "David Harbour",
+    "Robin Atkin Downes",
+    "Robert B. Kennedy",
+    "Billy Otis",
+    "Shailyn Pierre-Dixon",
+    "Jared Leto",
+    "James McGowan",
+    "Jim Parrack",
+    "Derek Perks",
+    "Jai Courtney",
+    "Ezra Miller",
+    "Jay Hernandez",
+    "Adewale Akinnuoye-Agbaje",
+    "Cara Delevingne",
+    "Joel Kinnaman",
+    "Aidan Devine",
+    "Andrew Bee",
+    "Clive McLean",
+    "Frank J. Zupancic",
+    "Kent Sheridan",
+    "Roger Shank",
+    "Dwight Ireland",
+    "Amanda Brugel",
+    "Peter Tufford Kennedy",
+    "Rosemary De Los Santos",
+    "Kevin Hanchard",
+    "Alain Chanoine",
+    "Joel Lacoursiere",
+    "Jameson Kraemer",
+    "Ho Chow",
+    "Matt Baram",
+    "Birgitte Solem",
+    "Scott Eastwood",
+    "Kevin Vance",
+    "Alex Meraz",
+    "Tyler Grey",
+    "Adam Beach",
+    "Nathan Brown",
+    "Karen Fukuhara",
+    "Kenneth Choi",
+    "Nicolas Van Burek",
+    "Agueda Cardenas",
+    "Corina Calderon",
+    "Daniela Uruena",
+    "Nicolas Uruena"
+  ]
+})
 
-function pushArray(array, array2){
-  array.push.apply(array, array2)
-}
 
-for (i = 1; i < 6; ++i) {
-  favMovies.pushArray(favMovies, ("movies" + i))
-}
+sortedFavMovies = favMovies.sort(function (a, b) {
+  var titleA = a.title.toUpperCase(); // ignore upper and lowercase
+  var titleB = b.title.toUpperCase(); // ignore upper and lowercase
+  if (titleA < titleB) {
+    return -1;
+  }
+  if (titleA > titleB) {
+    return 1;
+  }
 
+  // names must be equal
+  return 0;
+});
 
+console.log(sortedFavMovies)
 
+var json = JSON.stringify(sortedFavMovies);
 
-
-
-console.log(favMovies)
+console.log(json);
